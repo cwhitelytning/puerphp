df506279aff438c44686b636e32d06df817b94b0
Сlay Whitelytning <cwhitelytning@gmail.com>
2022-10-25 20:47:45 +0300

    Module factory has been removed

--- engine/Engine.php
+++ engine/Engine.php
@@ -1,8 +1,6 @@
 <?php namespace engine;
 
 include_once('includes/module/Module.inc');
-include_once('includes/factory/ModuleFactory.inc');
-
 include_once('includes/plugin/Plugin.inc');
 include_once('includes/setti/ConfigFile.inc');
 include_once('includes/loader/LocalModuleLoader.inc');
