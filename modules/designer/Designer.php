82d3102c7674fdf7b1a9c43e813c71f30c1982e9
Clay Whitelytning <cwhitelytning@gmail.com>
2022-10-29 20:51:29 +0300

    Added a module that allows you to dynamically create pages using XML and HTML markup.

---
+++ modules/designer/Designer.php
@@ -0,0 +1,27 @@
+<?php namespace modules\designer;
+
+include_once('src/EntityInterface.inc');
+include_once('src/Node.inc');
+
+include_once('src/xml/XMLEntity.inc');
+include_once('src/xml/XMLString.inc');
+include_once('src/xml/XMLComment.inc');
+include_once('src/xml/XMLDocument.inc');
+include_once('src/dom/DOMDocument.inc');
+include_once('src/html/HTMLDocument.inc');
+
+use core\src\module\Module;
+use core\src\module\ModuleInfo;
+
+#[
+  ModuleInfo
+  (
+    name: 'Designer',
+    author: 'Clay Whitelytning',
+    version: '1.1.3',
+    description: 'Dynamic page creation using XML and HTML markup'
+  )
+]
+final class Designer extends Module
+{
+}
